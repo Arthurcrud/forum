be70736bbd96e0d6dc45288c708d62676a769394
Patrick <patrickdev4@gmail.com>
2024-05-02 10:01:29 -0300

    refactor: altera arquivos e adiciona conexao com banco

---
+++ includes/message.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($_SESSION['message'])) {
+?>
+  <script>
+    window.onload = function() {
+      M.toast({
+        html: '<?php echo $_SESSION['message']; ?>',
+        classes: "rounded teal"
+      });
+    }
+  </script>
+<?php
+}
